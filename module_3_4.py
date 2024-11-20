7b36d5a04152fb12acf7ef67c7eb6ac8f6c18aa8
aodeyn <aodeyn@gmail.com>
2024-11-20 23:20:33 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,14 @@
+def single_root_words (root_word, *other_words):
+    same_words = []
+    n = len(other_words)
+    for i in range(n):
+        if other_words[i].lower().find(root_word.lower()) > -1 or  root_word.lower().find(other_words[i].lower())>-1:
+            same_words.append(other_words[i])
+    return  same_words
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
+
+
+
